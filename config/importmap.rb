c6f0593618a844a0696183ad64f7d43f137e64e3
Victor Malukhin <victor.malukhin@gmail.com>
2024-06-09 14:37:07 +0300

    Add bootstrap 5.3.3

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,8 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "@popperjs/core", to: "popper.js", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
